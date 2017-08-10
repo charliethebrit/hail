773fba2cc9096a8add68b21806c5947ca1067630
Charles Colley <charles.colley@tufts.edu>
2017-08-10 12:07:35 -0400

    addressed formatting comments

--- python/hail/dataset.py
+++ python/hail/dataset.py
@@ -4413,7 +4413,7 @@ class VariantDataset(object):
     @typecheck_method(key_name=strlike,
                       variant_keys=strlike,
                       single_key=bool,
-                      weight_expr=nullable(strlike),
+                      weight_expr=None,
                       y=strlike,
                       covariates=listof(strlike),
                       use_dosages=bool)
@@ -4501,8 +4501,8 @@ class VariantDataset(object):
                            generated to from a beta distribution (alpha = 1, beta = 25) evalulated at the minor allele
                            frequency of each variant.
 
-        :param covariates: list of covariate expressions.
-        :type covariates: list of str
+        :param covariates: List of covariate expressions.
+        :type covariates: List of str
 
         :param bool use_dosages: If true, use dosage genotypes rather than hard call genotypes.
 
